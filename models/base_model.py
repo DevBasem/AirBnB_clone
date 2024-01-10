addb939f428518c7d16a51c3226339a5c211b59c
DevBasem <engr.basemahmed@gmail.com>
2024-01-10 20:47:51 +0200

    task 1

---
+++ models/base_model.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+Module for the BaseModel class.
+"""
+import uuid
+from datetime import datetime
+
+class BaseModel:
+    """
+    The BaseModel class for handling common attributes/methods for other classes.
+    """
+
+    def __init__(self, *args, **kwargs):
+        """Initialize a new instance of BaseModel."""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                elif key == 'created_at' or key == 'updated_at':
+                    setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
+    def save(self):
+        """Update the public instance attribute updated_at with the current datetime."""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Return a dictionary representation of the instance."""
+        new_dict = self.__dict__.copy()
+        new_dict['created_at'] = self.created_at.isoformat()
+        new_dict['updated_at'] = self.updated_at.isoformat()
+        new_dict['__class__'] = self.__class__.__name__
+        return new_dict
+
+    def __str__(self):
+        """Return the string representation of the instance."""
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.to_dict())
