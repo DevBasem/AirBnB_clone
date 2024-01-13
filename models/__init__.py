ed574905c51b67c222ca14ef8a37686e4b6ca6f5
DevBasem <engr.basemahmed@gmail.com>
2024-01-13 14:05:01 -0800

    check 12

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
 """
-Module: __init__.py
+__init__.py: Package initializer
+
+Description:
+    - Initializes the package with a FileStorage instance
+    from the models.engine module.
+    - It triggers a reload operation, loading existing data
+    from file.json into memory.
 """
+
+
 from models.engine import file_storage
 
 storage = file_storage.FileStorage()
