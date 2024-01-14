052b46c6ca60101db1909ac8eb01d415b28dd00d
DevBasem <engr.basemahmed@gmail.com>
2024-01-13 17:43:19 -0800

    check 18

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -168,11 +168,12 @@ class TestFileStorage_methods(unittest.TestCase):
         self.assertIn("Amenity." + am.id, objs)
         self.assertIn("Review." + rv.id, objs)
 
-
     def test_reload_with_arg(self):
         with self.assertRaises(TypeError):
             models.storage.reload(None)
+
     def test_reload_missing_file(self):
+        # Check that reload raises FileNotFoundError
         with self.assertRaises(FileNotFoundError):
             models.storage.reload()
 
